c7fffecb2dd6b3bce94b6d9e30e5030f10895491
LaimisKirsa <laimisa@gmail.com>
2024-02-29 17:36:50 +0200

    imported Components, created design layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
+import Item from './components/item/Item';
+import data from './assets/product-data/products'
 
 function App() {
-
   return (
     <>
-      
+      <div className="container">
+        {data.products.map((product, index) => 
+          <Item key={index} item={product} />
+        )}
+      </div>
     </>
   )
 }
